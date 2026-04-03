485e7ec3a663166231f0e9522c39ae4185bc4dd4
Julian Rosse <julian@helixbass.net>
2026-04-03 18:49:50 -0400

    true or false parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
     println!("Hello, world!");
 }
+
+
+fn parser_truefalse(file_content: &[u8]) -> bool {
+    if file_content.len() < 1 {
+        return false
+    } else {
+        return true
+    }
+
+
+}
